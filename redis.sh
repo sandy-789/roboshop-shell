01195623c5044e3ad281de527f2adc3974bb2e92
Sandeep Vadla <sandeep_vadla@merilytics.com>
2023-12-20 15:13:06 +0530

    setup mongodb

--- redis.sh
+++ redis.sh
@@ -39,7 +39,7 @@ VALIDATE $? "enabling redis"
 
 dnf install redis -y
 
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis.conf
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis/redis.conf
 
 VALIDATE $? "Allowing remote connection"
 
